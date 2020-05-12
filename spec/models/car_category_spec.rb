a9aeae981c2725eb52927e49a84697a60f0623a6
Dmadaschi <daniel@vetwork.com.br>
2020-05-12 19:13:51 -0300

    add symbolize_names ro json parser

--- spec/models/car_category_spec.rb
+++ spec/models/car_category_spec.rb
@@ -9,24 +9,29 @@ RSpec.describe CarCategory, type: :model do
     it { should validate_uniqueness_of(:name) }
     it { should validate_numericality_of(:daily_rate).is_greater_than(0) }
     it { should validate_numericality_of(:car_insurance).is_greater_than(0) }
-    it { should validate_numericality_of(:third_part_insurance)
-                 .is_greater_than(0) }
+    it { should validate_numericality_of(:third_part_insurance).is_greater_than(0) }
   end
-  
+
   context 'references' do
-    it { should have_many(:car_models)}
+    it { should have_many(:car_models) }
   end
 
   context 'validate messages of' do
     it 'presence' do
-      car_category = CarCategory.new()
+      car_category = CarCategory.new
 
       car_category.save
 
-      expect(car_category.errors[:name]).to include("não pode ficar em branco")
-      expect(car_category.errors[:daily_rate]).to include("não pode ficar em branco")
-      expect(car_category.errors[:car_insurance]).to include("não pode ficar em branco")
-      expect(car_category.errors[:third_part_insurance]).to include("não pode ficar em branco")
+      expect(car_category.errors[:name]).to include('não pode ficar em branco')
+      expect(car_category.errors[:daily_rate]).to include(
+        'não pode ficar em branco'
+      )
+      expect(car_category.errors[:car_insurance]).to include(
+        'não pode ficar em branco'
+      )
+      expect(car_category.errors[:third_part_insurance]).to include(
+        'não pode ficar em branco'
+      )
     end
 
     it 'uniqueness' do
@@ -35,17 +40,25 @@ RSpec.describe CarCategory, type: :model do
 
       car_category.save
 
-      expect(car_category.errors[:name]).to include("já está em uso")
+      expect(car_category.errors[:name]).to include('já está em uso')
     end
 
     it 'greater_than' do
-      car_category = build(:car_category, daily_rate: '0',
-                           car_insurance: '0', third_part_insurance: '0')
-      car_category.save                               
+      car_category = build(:car_category,
+                           daily_rate: '0',
+                           car_insurance: '0',
+                           third_part_insurance: '0')
+      car_category.save
 
-      expect(car_category.errors[:daily_rate]).to include("deve ser maior que 0")
-      expect(car_category.errors[:car_insurance]).to include("deve ser maior que 0")
-      expect(car_category.errors[:third_part_insurance]).to include("deve ser maior que 0")
+      expect(car_category.errors[:daily_rate]).to include(
+        'deve ser maior que 0'
+      )
+      expect(car_category.errors[:car_insurance]).to include(
+        'deve ser maior que 0'
+      )
+      expect(car_category.errors[:third_part_insurance]).to include(
+        'deve ser maior que 0'
+      )
     end
   end
 end
--- spec/requests/car_management_spec.rb
+++ spec/requests/car_management_spec.rb
@@ -5,14 +5,27 @@ describe 'car management' do
     it 'renders available cars' do
       cars = create_list(:car, 3, status: :available)
       rented_cars = create_list(:car, 3, status: :rented)
+
       get api_v1_cars_path
 
-      response_json = JSON.parse(response.body, simbolize_names: true)
+      response_json = JSON.parse(response.body, symbolize_names: true)
       expect(response).to have_http_status(:ok)
       expect(response.content_type).to include('application/json')
-      expect(response_json[0]['license_plate']).to eq(cars.first.license_plate)
-      expect(response_json[1]['license_plate']).to eq(cars.second.license_plate)
-      expect(response_json[2]['license_plate']).to eq(cars.third.license_plate)
+      expect(response_json[0][:license_plate]).to eq(cars.first.license_plate)
+      expect(response_json[1][:license_plate]).to eq(cars.second.license_plate)
+      expect(response_json[2][:license_plate]).to eq(cars.third.license_plate)
+      expect(response_json[0][:car_model_id]).to eq(cars.first.car_model_id)
+      expect(response_json[1][:car_model_id]).to eq(cars.second.car_model_id)
+      expect(response_json[2][:car_model_id]).to eq(cars.third.car_model_id)
+      expect(response_json[0][:mileage]).to eq(cars.first.mileage)
+      expect(response_json[1][:mileage]).to eq(cars.second.mileage)
+      expect(response_json[2][:mileage]).to eq(cars.third.mileage)
+      expect(response_json[0][:color]).to eq(cars.first.color)
+      expect(response_json[1][:color]).to eq(cars.second.color)
+      expect(response_json[2][:color]).to eq(cars.third.color)
+      expect(response_json[0][:status]).to eq('available')
+      expect(response_json[1][:status]).to eq('available')
+      expect(response_json[2][:status]).to eq('available')
       expect(response_json).to_not include(rented_cars)
     end
 
